a1f8f6c3d14707a0c931c6a951c2681e39f1b81a
Nicolas Wormser <nicolas@codewithflair.org>
2023-03-26 21:34:40 +0200

    Refactor + add search

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
-import { createCard } from "./createCard.js";
-import houses from "./houseToRent.js";
+import { createCard } from "./createCard.js"
+import houses from "./houseToRent.js"
 
-const displayHouses = (houses) => {
-  const container = document.querySelector(".cards");
-  container.replaceChildren(...houses.map(createCard));
-};
+const displayHouses = houses => {
+  const container = document.querySelector(".cards")
+  container.replaceChildren(...houses.map(createCard))
+}
 
-const render = () => {
+const handleShowOnlyAvailable = () => {
   const showOnlyAvailableCheckbox = document.querySelector(
     ".available-checkbox"
-  );
+  )
+  showOnlyAvailableCheckbox.addEventListener("click", event => {
+    const isChecked = event.target.checked
+    displayHouses(isChecked ? houses.filter(h => h.available === true) : houses)
+  })
+}
 
-  showOnlyAvailableCheckbox.addEventListener("click", (event) => {
-    if (event.target.checked) {
-      displayHouses(houses.filter((h) => h.available === true));
-    } else {
-      displayHouses(houses);
-    }
-  });
+const handleFilterType = () => {
+  const filterTypeOption = document.querySelector(".select")
+  filterTypeOption.addEventListener("change", event => {
+    const type = event.target.value
+    const isAll = type === "All"
+    displayHouses(isAll ? houses : houses.filter(h => h.type === type))
+  })
+}
 
-  displayHouses(houses);
-};
+const handleFilterFromSearch = () => {
+  const searchInput = document.querySelector(".search-input")
+  searchInput.addEventListener("input", event => {
+    const text = event.target.value
+    const results = houses.filter(h => h.name.toLowerCase().includes(text.toLowerCase()))
+    displayHouses(results)
+  })
+}
 
-render();
+const render = () => {
+  handleFilterType()
+  handleShowOnlyAvailable()
+  handleFilterFromSearch()
+  displayHouses(houses)
+}
+
+render()
